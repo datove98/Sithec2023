eff2d310a734a5783760822f5f7cb9dfa0d42b77
Daniel Torres Verdines <danieltorres@bakerrobotics.com>
2023-03-24 02:24:25 -0600

    Agregado el controlador de Alumno

---
+++ Controllers/AlumnoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sithec2023.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlumnoController : ControllerBase
+    {
+    }
+}
